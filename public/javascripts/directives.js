44bef60b49f148729d98ed44dc7bce38b54b29bc
陈旭东 <dong@chenxudongdeMacBook-Air.local>
2017-08-07 15:26:55 +0800

    ##

--- public/javascripts/directives.js
+++ public/javascripts/directives.js
@@ -111,11 +111,11 @@ directive('documentViewer', function () {
                     InitViewMode : 'Portrait',
                     RenderingOrder : 'flash',
                     StartAtPage : '',
-                    ViewModeToolsVisible : false,
-                    ZoomToolsVisible : false,
-                    NavToolsVisible : false,
-                    CursorToolsVisible : false,
-                    SearchToolsVisible : false,
+                    ViewModeToolsVisible : true,
+                    ZoomToolsVisible : true,
+                    NavToolsVisible : true,
+                    CursorToolsVisible : true,
+                    SearchToolsVisible : true,
                     WMode : 'window',
                     localeChain: 'zh_CN'
                 }}
