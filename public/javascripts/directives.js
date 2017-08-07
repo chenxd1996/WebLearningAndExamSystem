f3d9ce74205bb88237acb591d52c8a357f5d9392
陈旭东 <dong@chenxudongdeMacBook-Air.local>
2017-08-07 21:38:49 +0800

    ##

--- public/javascripts/directives.js
+++ public/javascripts/directives.js
@@ -122,7 +122,6 @@ directive('documentViewer', function () {
             );
             $('#documentViewer').bind('onDocumentLoaded',function(e, totalPages){
                 console.log($FlexPaper('documentViewer').getTotalPages());
-                console.log(totalPages);
             });
             $('#documentViewer').bind('onCurrentPageChanged',function(e,pagenum){
                 if ($scope.pages.indexOf(pagenum) < 0) {
