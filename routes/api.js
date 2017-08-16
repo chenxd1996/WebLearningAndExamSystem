27da18068e58e9f5bbb5cf216152041df87a868d
陈旭东 <dong@chenxudongdeMacBook-Air.local>
2017-08-16 08:57:19 +0800

    ##

--- routes/api.js
+++ routes/api.js
@@ -1940,7 +1940,7 @@ exports.deleteCourse = function (req, res) {
                     status: false
                 });
             } else {
-                if (fs.statSync('public/CourseWares/' + course.cname).isDirectory()) {
+                if (fs.existSync('public/CourseWares/' + course.cname)) {
                     rimraf('public/CourseWares/' + course.cname, function (err) {
                         if (err) {
                             console.log("Delete CourseWare in deleteCourse: " + err);
