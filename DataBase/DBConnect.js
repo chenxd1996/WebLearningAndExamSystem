5edfdae4c2e80ba0bb051b630f6664cc1a029f8f
陈旭东 <dong@chenxudongdeMacBook-Air.local>
2017-08-19 09:43:59 +0800

    ##

--- DataBase/DBConnect.js
+++ DataBase/DBConnect.js
@@ -19,7 +19,21 @@ con.connect(function (err) {
 con.on('error', function (err) {
     console.log("Mysql connection exists err: " + err);
     if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-        con.connect();
+        con = mysql.createConnection({
+            host: 'localhost',
+            user: 'LearningAndExamSystem',
+            password: 'qweasd123',
+            database: 'LearningAndExamSystem',
+            port: '3306',
+            multipleStatements: true
+        });
+        con.connect(function (err) {
+            if (err) {
+                console.log("Can't connect to the database!");
+            } else {
+                console.log("Connected to the database!");
+            }
+        });
     } else {
         console.log("Disconnected to the database during run time: " +  err);
     }
