e338ab6a0d5bba052838f5b453bfe492278220bf
chenxd <1164542190@qq.com>
2018-03-19 10:16:06 +0800

    尝试修复数据库连接错误

--- DataBase/DBConnect.js
+++ DataBase/DBConnect.js
@@ -1,43 +1,45 @@
 var mysql = require("mysql");
-var con = mysql.createConnection({
-    host: process.env.DB_ADDR || 'localhost',
-    user: 'LearningAndExamSystem',
-    password: 'qweasd123',
-    database: 'LearningAndExamSystem',
-    port: process.env.DB_PORT || '3306',
-    multipleStatements: true
-});
 
-con.connect(function (err) {
-    if (err) {
-        console.log("Can't connect to the database!");
-    } else {
-        console.log("Connected to the database!");
-    }
-});
+function createConnection() {
+    var con = mysql.createConnection({
+        host: process.env.DB_ADDR || 'localhost',
+        user: 'LearningAndExamSystem',
+        password: 'qweasd123',
+        database: 'LearningAndExamSystem',
+        port: process.env.DB_PORT || '3306',
+        multipleStatements: true
+    });
+    return con;
+}
 
-con.on('error', function (err) {
-    console.log("Mysql connection exists err: " + err);
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-        con = mysql.createConnection({
-            host: process.env.DB_ADDR || 'localhost',
-            user: 'LearningAndExamSystem',
-            password: 'qweasd123',
-            database: 'LearningAndExamSystem',
-            port: process.env.DB_PORT || '3306',
-            multipleStatements: true
-        });
-        con.connect(function (err) {
-            if (err) {
-                console.log("Can't connect to the database!");
-            } else {
-                console.log("Connected to the database!");
-            }
-        });
-    } else {
-        console.log("Disconnected to the database during run time: " +  err);
-    }
-});
+var con = createConnection();
+
+function connect(con) {
+    con.connect(function (err) {
+        if (err) {
+            console.log("Can't connect to the database!");
+        } else {
+            console.log("Connected to the database!");
+        }
+    });
+}
+
+connect(con);
+
+function listenError(con) {
+    con.on('error', function (err) {
+        console.log("Mysql connection exists err: " + err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            con = createConnection();
+            connect(con);
+            listenError(con);
+        } else {
+            console.log("Disconnected to the database during run time: " +  err);
+        }
+    });
+}
+
+listenError(con);
 
 exports.getCon = function () {
     return con;
