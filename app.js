b34d2fd441c6559298a4cf8ff096481079143644
chenxd <1164542190@qq.com>
2018-04-25 10:16:25 +0800

    增加express能够接受请求大小，防止413错误

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ "limit":"10000kb"}));  //据需求更改limit大小
+app.use(bodyParser.urlencoded({"limit":"10000kb", extended: false})); //根据需求更改limit大小
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
